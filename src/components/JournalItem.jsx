0ae2696c4c0209e1f004da96ae1f35b7a659bc87
Bett007 <bettbrian022@gmail.com>
2025-11-25 22:14:20 +0300

    JournalItem component update

--- src/components/JournalItem.jsx
+++ src/components/JournalItem.jsx
@@ -1,12 +1,7 @@
 function JournalItem({ entry, toggleImportant }) {
   return (
     <div
-      style={{
-        border: "1px solid #ccc",
-        padding: "10px",
-        marginBottom: "10px",
-        backgroundColor: entry.important ? "#fff9c4" : "white",
-      }}
+      className={`journal-item ${entry.important ? "important" : ""}`}
     >
       <h3>
         {entry.title}{" "}
